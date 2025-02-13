540fca4565408913121b5ff3cc18494252521a78
colls@cradlevoices.com <cradle-voices>
2025-02-13 21:28:01 +0300

    update url

--- merchants/routes.go
+++ merchants/routes.go
@@ -1047,7 +1047,7 @@ func TagsHandler(c *gin.Context) {
 
 	// Print the Base64 encoded string
 	fmt.Println("Base64 Encoded Data:", encoded)
-	cardlink := "https://mpgs.cradlevoices.com/mpgs.php?data=" + encoded
+	cardlink := "https://mpgs.mhs.com/mpgs.php?data=" + encoded
 
 	c.JSON(http.StatusOK, gin.H{
 		"message":  "card Payment  initiation successful",
