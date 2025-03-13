a614baa0be2802689ce7614a7d9c1d7597462e03
colls@cradlevoices.com <cradle-voices>
2025-03-13 16:52:56 +0300

    added logs

--- merchants/routes.go
+++ merchants/routes.go
@@ -1003,21 +1003,21 @@ func CardCallbackHandler(c *gin.Context) {
 	}
 	//testing
 	// Convert struct to JSON (map representation)
-	jsonData, _ := json.Marshal(callbackBody)
+	// jsonData, _ := json.Marshal(callbackBody)
 
-	// Convert JSON to map[string]interface{}
-	var callbackMap map[string]interface{}
-	json.Unmarshal(jsonData, &callbackMap)
+	// // Convert JSON to map[string]interface{}
+	// var callbackMap map[string]interface{}
+	// json.Unmarshal(jsonData, &callbackMap)
 
-	// Print key-value pairs
-	for key, value := range callbackMap {
-		fmt.Printf("%s: %v\n", key, value)
-	}
+	// // Print key-value pairs
+	// for key, value := range callbackMap {
+	// 	fmt.Printf("%s: %v\n", key, value)
+	// }
 
 	// Extract the MerchantRequestID from the RedirectURL
-	merchantRequestID := callbackBody.SecureID
-	fmt.Println("callback data: ", callbackBody)
-	fmt.Print("merchantRequestID: ", callbackBody.SecureID)
+	merchantRequestID := callbackBody.RedirectURL
+	// fmt.Println("callback data: ", callbackBody)
+	// fmt.Print("merchantRequestID: ", callbackBody.SecureID)
 
 	db := database.GetConnection()
 
