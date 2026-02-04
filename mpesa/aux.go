5e818cb2506c103fa5bc60abc65ac7bcdb30d4b6
hashcript <collscodes@gmail.com>
2026-02-04 12:30:34 +0300

    withdrawal callback

--- mpesa/aux.go
+++ mpesa/aux.go
@@ -280,8 +280,7 @@ func GenerateB2CRequest(phoneNumber string, amount float64, callbackURL, externa
 		Remarks:                  "payments done",
 		QueueTimeOutURL:          "https://payments.mam-laka.com/api/v1/mobile/b2c/callback",
 		ResultURL:                "https://payments.mam-laka.com/api/v1/mobile/b2c/callback",
-		// ResultURL: "https://webhook.site/b67e9e96-b44c-4880-95ee-6ea2519a65ee",
-		// ResultURL: "https://webhook.site/c7edfd71-ae2e-4a15-9d7a-c4132c05e746",
+		
 
 		Occassion: "Ok",
 	}
