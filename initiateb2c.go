11a8187c5c3c5877e01a27b7ce884568d9250969
hashcript <collscodes@gmail.com>
2026-04-28 17:49:37 +0300

    reverted to main.go

--- initiateb2c.go
+++ initiateb2c.go
@@ -186,7 +186,7 @@ func checkTransactionStatus(transactionID string) (map[string]interface{}, error
 	return pretty, nil
 }
 
-func main88() {
+func main88000() {
 	// r := gin.Default()
 
 	// B2C Payment
--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func Migration(database *gorm.DB) {
 	settlement.AutoMigrate()
 }
 
-func main999() {
+func main() {
 	//load the env
 	err := godotenv.Load()
 	if err != nil {
